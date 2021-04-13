f6f14e9fdb4a64c4d1435addcaa8b3f1a6649f59
23232333aaa <68760191+23232333aaa@users.noreply.github.com>
2021-04-13 18:37:13 +0800

    Add files via upload

--- 密碼.py
+++ 密碼.py
@@ -5,5 +5,7 @@ ls3='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 a=random.sample(ls,5)
 b=random.sample(ls2,5)
 c=random.sample(ls3,5)
-for Pass in a+b+c:
-    print(Pass,end='')
+Pass=a+b+c
+random.shuffle(Pass)
+for s in Pass:
+    print(s,end='')
